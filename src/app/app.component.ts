13901f64c803fd8cbd9cd7a91d6d6ee33b533db1
Lenovo-PC\Danylo <danilogoryachko@gmail.com>
2020-09-19 01:30:17 +0300

    create project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'nasa-app';
+  apiKey = 'KDOO4Edhzs3Y3l2beMGuiyASkKy39Ta3jJrzIvAI';
 }
